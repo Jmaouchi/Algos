3d2521c07aa084375f3a4bccf239b1e656e51bd1
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2023-05-30 23:30:15 -0500

    find odd num that is been repeated only one time inside the array

---
+++ findOddNum.js
@@ -0,0 +1,30 @@
+const array = [1,2,2,3,3,3,4,3,3,3,2,2,1]
+let odd = {}
+
+function findOdd(A) {
+
+
+  // check how many times a number repeats
+  for(let i = 0; i<A.length; i++) {
+    // if the odd object has a reference to the A array, then add 1 to its key (like: value : key or 2 : 1)
+    if(odd[A[i]]) {
+      odd[A[i]]++;
+    // if the odd object doest have a reference to it, then create it and set it to 1
+    }else{
+      odd[A[i]] = 1
+    }
+  }
+
+  // for in loop 
+  for(let key in odd) { // if the key is in the odd object then call then =>
+    // check if the that key % 2 is not equal to 0 and that means that the number is only repeated once 
+    if(odd[key] % 2 !== 0) {
+      console.log(Number(key));
+    }
+  }
+
+  console.log(odd);
+}
+
+findOdd(array)
+
